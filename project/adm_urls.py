952d70380f2583a3a62d1c331aa9fa2355efca46
Micael Farias <micaelfarias.dev@gmail.com>
2025-03-24 10:13:01 -0300

    modificação no site, melhorias no wep app

--- project/adm_urls.py
+++ project/adm_urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 from django.conf.urls.static import static
+from .views import redadm
 
 app_name = 'adm'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', redadm),
     path('', include('empresa.urls'), name='adm'),
 ]
 
--- project/views.py
+++ project/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import redirect
 
 def red(request, cod):
     return redirect(f'/cliente/pedido/?codigo={cod}')
+
+def redadm(request):
+    return redirect('adm:empresa')
